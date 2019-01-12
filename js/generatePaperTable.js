c6e5594d16e107228a51c3272c6aa9d9a2f88e1d
Jinki Jung <hypnotize@kaist.ac.kr>
2019-01-12 23:23:11 +0900

    Update generatePaperTable.js

--- js/generatePaperTable.js
+++ js/generatePaperTable.js
@@ -1,12 +1,18 @@
 
 $(document).ready(function() {
+  $.ajax({
+     type: "GET",
+     url: "https://raw.githubusercontent.com/VirtualityForSafety/SharePaper/master/metadata/columns.csv",
+     dataType: "text",
+     success: function(csvData) {
+         columnDescription = generatePaperColumn(readCSV(csvData));
+       }
+});
      $.ajax({
         type: "GET",
         url: "https://raw.githubusercontent.com/VirtualityForSafety/SharePaper/master/metadata/papers.csv",
         dataType: "text",
         success: function(csvData) {
-            columnDescription = generatePaperColumn(readCSV(csvData));
-            console.log(columnDescription);
             document.getElementById("paper").innerHTML = generatePaperTable(readCSV(csvData)); }
      });
      $('.show_hide').click(function(){
