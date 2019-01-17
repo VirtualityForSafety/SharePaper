8cdf8015f0b1e89b5eaeb86683f370f7c6c8dfa1
Jinki Jung <hypnotize@kaist.ac.kr>
2019-01-17 10:53:42 +0900

    adjust top link and style

---
+++ js/topLink.js
@@ -0,0 +1,7 @@
+function insertTopLink(){
+  var shareButton ="<input class=\"top_share\" type=\"button\" value=\"Share \" onClick=\"location.href='index.html'\">";
+  var paperButton ="<input class=\"top_paper\" type=\"button\" value=\" Paper\" onClick=\"location.href='tag.html'\">";
+  return shareButton+paperButton+"<br>";
+}
+
+document.getElementById("topLink").innerHTML = insertTopLink();
