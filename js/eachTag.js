c248ee439a986a7088654f54ccef8293d364fdc3
Jinki Jung <hypnotize@kaist.ac.kr>
2019-01-22 22:48:25 +0900

    minor bug fixed

--- js/eachTag.js
+++ js/eachTag.js
@@ -1,10 +1,22 @@
-// from http://jsfiddle.net/mblase75/dcqxr/
+function getProjectNameFromLink(){
+  var element = window.location.href.split('?');
+  if(element.length>1){
+    var item = element[element.length -1].split('&');
+    for(var i=0; i<item.length ; i++){
+      var subItem = item[i].split('=');
+      if(subItem[0] == 'proj')
+        return subItem[1];
+    }
+  }
+  return undefined;
+}
 
+var projectName = getProjectNameFromLink();
 
 $(document).ready(function() {
      $.ajax({
         type: "GET",
-        url: "https://raw.githubusercontent.com/VirtualityForSafety/SharePaper/master/metadata/papers.csv",
+        url: "metadata/"+projectName+"/papers.csv",
         dataType: "text",
         success: function(data) {
             document.getElementById("checkTag").innerHTML = processCSVData(data); }//prepareCollapsible();}
