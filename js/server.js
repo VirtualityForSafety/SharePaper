40907e44826f8f25fa7fdfedec37dc6c77fdc5f8
otterlee <21700525@handong.edu>
2019-01-23 00:14:48 -0700

    convert timestamp to local time

--- js/server.js
+++ js/server.js
@@ -13,7 +13,7 @@ function zeroPad(nr,base){
 
 function getCurrentTime(){
   var date = new Date();
-  //date = convertUTCDateToLocalDate(date);
+  var localDate = convertUTCDateToLocalDate(date);
   console.log(date.toString());
   var datevalues = [
    zeroPad(date.getFullYear(),1000),
@@ -23,7 +23,7 @@ function getCurrentTime(){
    zeroPad(date.getMinutes(),10),
    zeroPad(date.getSeconds(),10)
 ];
-  return date.toString();
+  return date.toString() + localDate.toString();
 }
 
 function convertUTCDateToLocalDate(date) {
