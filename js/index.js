34363792565737fd111527096324276db70fdc67
Jinki Jung <hypnotize@kaist.ac.kr>
2019-01-23 11:43:13 +0900

    project listing to index page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+$(document).ready(function() {
+    $.ajax({
+       type: "GET",
+       url: "metadata/proj.csv",
+       dataType: "text",
+       success: function(csvData) {
+           document.getElementById("projectList").innerHTML = getProject(csvData); }
+    });
+});
+
+function getRawText(data){
+  return data.replace(" ","").replace("/","").toLowerCase();
+}
+
+function getProject(csvData){
+  var result ="<table width=90%>";
+  var data = parseText(csvData);
+  for(var i=1 ; i<data.length ; i++){
+    result += "<tr><td><div OnClick=\"location.href='share.html?proj="+getRawText(data[i][1])+"'\"><h3>"+data[i][1]+"</h3>"+data[i][2]+"</td></div></tr>";
+  }
+  result += "</table>";
+  return result;
+}
+
 function setValueToLS(value){
   if(typeof(Storage)!=="undefined")
   {
