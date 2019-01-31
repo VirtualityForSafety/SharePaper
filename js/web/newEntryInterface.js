f0ef1250c576986c98ff37147f145870d9dc412b
Jinki Jung <hypnotize@kaist.ac.kr>
2019-01-31 09:15:09 +0900

    update paper upload form

--- js/web/newEntryInterface.js
+++ js/web/newEntryInterface.js
@@ -34,25 +34,83 @@ function generateNewEntry(){
   return result;
 }
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function drawConfirmCircle(color) {
+    var canvas = document.getElementById("imgCanvas");
+    var context = canvas.getContext("2d");
+    var rect = canvas.getBoundingClientRect();
+    var posx = e.clientX - rect.left;
+    var posy = e.clientY - rect.top;
+
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(posx, posy, 50, 0, 2 * Math.PI);
+    context.fill();
+}
+
 function generateNewEntryCore(){
   //var result += "<p>Some text in the Modal..</p>";
   // for new entry
-  var result = "<p >";
+  var result = "<table>";
   //*
   headers = ['id','title', 'year', 'journalconference', 'author', 'keyword', 'quality', 'summary', 'timestamp', 'contributor', 'link'];
-  for(var k=0; k<headers.length-1; k++){
-    var textValue="";
-    if(k+1==9)
-      result +="<textarea id=\"new_paper_"+headers[k+1]+"\" cols=\"20\">"+getValueFromLS()+"</textarea></br>";
-    else if(k+1==8)
-      result +="<textarea style=\"display:none;\" id=\"new_paper_"+headers[k+1]+"\" cols=\"20\"></textarea>";
-    else if(k+1==10)
-      result +=""+createPopup()+"";
-    else
-      result +="<textarea id=\"new_paper_"+headers[k+1]+"\" cols=\"20\">"+textValue+"</textarea></br>";
+  for(var k=1; k<headers.length; k++){
+    if(headers[k]=='timestamp' || headers[k]=='link')
+      continue;
 
+    result +="<tr>";
+    var textValue="";
+    if(k==9)
+      textValue = getValueFromLS();
+
+    result += "<td class=table_title width=10><b>"+capitalizeFirstLetter(headers[k])+"</b></td>";
+    if(k==1){
+      result +="<td><textarea onBlur=\"checkEntry("+"'new_paper_"+headers[k]+"');\" style=\"border: none; width: 100%; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;\" id=\"new_paper_"+headers[k]+"\" cols=\"20\">"+textValue+"</textarea></td>";
+    }
+
+    else if(k==9)
+      result +="<td><textarea onBlur=\"checkEntry("+"'new_paper_"+headers[k]+"');\" style=\"border: none; width: 100%; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;\" id=\"new_paper_"+headers[k]+"\" cols=\"20\">"+textValue+"</textarea></td>";
+    else if(k==8)
+      result +="<td><textarea onBlur=\"checkEntry("+"'new_paper_"+headers[k]+"');\" style=\"display:none;\" id=\"new_paper_"+headers[k]+"\" cols=\"20\"></textarea></td>";
+    else if(k==10)
+      result +="<td>"+createPopup()+"</td>";
+    else
+      result +="<td><textarea onBlur=\"checkEntry("+"'new_paper_"+headers[k]+"');\" style=\"border: none; width: 100%; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;\" id=\"new_paper_"+headers[k]+"\" cols=\"20\">"+textValue+"</textarea></td>";
+
+    if(textValue.length==0)
+      result += "<td width=2><img src=\"asset/undefined.png\" id=\"new_paper_"+headers[k]+"_img\" width=10 height=10 ></td>";
+    else {
+      result += "<td width=2><img src=\"asset/confirm.png\" id=\"new_paper_"+headers[k]+"_img\" width=10 height=10 ></td>";
+    }
+
+    if(k==1){
+      result +="</tr><tr><td colspan=3><button>Import bibtex</button><button>Apply bibtex</button></td></tr>";
+    }
+    result +="</tr>";
     // result += "<td><input type=\"button\" value=\"Submit\" onclick=\"passNewEntryParameter(99999)\">"+hiddenItem+"</td>";
   }
-  result += "</p>";
+  result += "<tr><td colspan=3><button id=\"submit_\" onclick=\"passNewEntryParameter('"+projectName+"','paper')\">Submit</button></td></tr>";
+  result += "</table>";
   return result;
+}
+
+function checkEntry(entered){
+  if($("#"+entered).val().length>0){
+    document.getElementById(entered+"_img").src = "asset/confirm.png";
+  }
+  else{
+    document.getElementById(entered+"_img").src = "asset/undefined.png";
+  }
+}
+
+function checkEntryOnline(entered){
+  if($("#"+entered).val().length>0){
+    document.getElementById(entered+"_img").src = "asset/confirm.png";
+  }
+  else{
+    document.getElementById(entered+"_img").src = "asset/undefined.png";
+  }
 }
